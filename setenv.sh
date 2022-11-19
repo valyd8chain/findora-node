f5bfb57c7a543670bf71df0a7e74fed876c380bc
Valyd8Chain <valyd8chain@protonmail.com>
2022-11-19 13:17:43 -0800

    - revert back to origin update link

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,4 @@
 
 export $(grep -v '^#' .env | xargs)
 export LIVE_VERSION=$(curl -s https://prod-mainnet.prod.findora.org:8668/version | awk -F\  '{print $2}')
-export CHAINDATA_URL="https://prod-mainnet-us-west-2-chain-data-backup.s3.us-west-2.amazonaws.com/mainnet-20221116010001.tar.gz"
+export CHAINDATA_URL=$(curl -s https://prod-${NAMESPACE}-us-west-2-chain-data-backup.s3.us-west-2.amazonaws.com/latest | awk -F , '{print $1}')
